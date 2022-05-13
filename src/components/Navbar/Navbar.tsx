14130180bbc9ef470ae325bd4444617d69c9f85f
Vinit Kanse <vinitkansend@gmail.com>
2022-05-13 16:54:48 +0530

    Fix: Nav icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import {AiFillTrophy} from 'react-icons/ai'
 import logo from "../../assets/logo.png";
 import { Account } from "../Account/Account";
 import { ThemeSwitcher } from "../ThemeSwitcher/ThemeSwitcher";
@@ -12,7 +13,10 @@ export const Navbar = () => {
             <img src={logo} alt="logo" className="img img-responsive" />
           </Link>
         </div>
+        <div className="nav-links">
+        <AiFillTrophy className="nav-icon" />
         <ThemeSwitcher />
+        </div>
         <Account />
       </div>
     </nav>
